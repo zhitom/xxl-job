7c18726561f6c58f178eccf371798536b2f32b66
shandy <shandyshandy@163.com>
2019-06-04 13:24:55 +0800

    update bug

--- doc/db/tables_xxl_job_for_oracle.sql
+++ doc/db/tables_xxl_job_for_oracle.sql
@@ -382,9 +382,9 @@ alter table xxl_job_lock
 ---------------------------------------------------------
 --DATA
 ---------------------------------------------------------
-INSERT INTO xxl_job_group(id, app_name, title, "order", address_type, address_list) VALUES (1, 'xxl-job-executor-sample', '示例执行器', 1, 0, NULL);
+INSERT INTO xxl_job_group(id, app_name, title, worder, address_type, address_list) VALUES (1, 'xxl-job-executor-sample', '示例执行器', 1, 0, NULL);
 INSERT INTO xxl_job_info(id, job_group, job_cron, job_desc, add_time, update_time, author, alarm_email, executor_route_strategy, executor_handler, executor_param, executor_block_strategy, executor_timeout, executor_fail_retry_count, glue_type, glue_source, glue_remark, glue_updatetime, child_jobid) VALUES (1, 1, '0 0 0 * * ? *', '测试任务1', to_date('2018-11-03 22:21:31','YYYY-MM-DD HH24:MI:SS'), to_date('2018-11-03 22:21:31','YYYY-MM-DD HH24:MI:SS'), 'XXL', '', 'FIRST', 'demoJobHandler', '', 'SERIAL_EXECUTION', 0, 0, 'BEAN', '', 'GLUE代码初始化', to_date('2018-11-03 22:21:31','YYYY-MM-DD HH24:MI:SS'), '');
-INSERT INTO xxl_job_user(id, "username", password, role, permission) VALUES (1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 1, NULL);
+INSERT INTO xxl_job_user(id, wusername, password, role, permission) VALUES (1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 1, NULL);
 INSERT INTO xxl_job_lock ( lock_name) VALUES ( 'schedule_lock');
 
 commit;
